fd742af3142e69c7d0babfa40549bb54acf82825
mhans003 <michaeledwardhanson@gmail.com>
2021-03-13 02:37:24 -0800

    Added extra information for bubble sort, including steps output and complexity/stability information.

--- js/sorts/Bubble.js
+++ js/sorts/Bubble.js
@@ -1,21 +1,29 @@
 function bubbleSort(arr) {
     //This will be set when a swap is made 
-    var noSwaps;
+    let noSwaps;
 
     //Loop through every item starting at the end.
     for(let endOfCompare = arr.length; endOfCompare > 0; endOfCompare--) {
-        //Assume this has no swaps made.
+        //Assume this round has no swaps made.
         noSwaps = true;
         //Loop through every unsorted item, starting at beginning, until the sorted section starts.
         for(let currentPlace = 0; currentPlace < endOfCompare - 1; currentPlace++) {
-            //Identify the 'next' place
+            //Identify the 'next' place to compare to the 'current' place.
             nextPlace = currentPlace + 1;
             //If the current place is greater than the next place, we want to switch them (bubble it up)
             if(arr[currentPlace] > arr[nextPlace]) {
                 //console.log(`checking ${arr[currentPlace]} and ${arr[nextPlace]}`);
+                /*
                 let temp = arr[currentPlace];
                 arr[currentPlace] = arr[nextPlace];
                 arr[nextPlace] = temp;
+                */
+                document.getElementById("steps").innerHTML += `<li>Bubbling up ${arr[currentPlace]}</li>`;
+                [arr[currentPlace], arr[nextPlace]] = [arr[nextPlace], arr[currentPlace]];
+                /* 
+                ES6 Version for swapping:
+                [arr[currentPlace], arr[nextPlace]] = [arr[nextPlace], arr[currentPlace]];
+                */
                 //There has been at least one swap, so we don't want to break out of this quite yet.
                 noSwaps = false;
             }
