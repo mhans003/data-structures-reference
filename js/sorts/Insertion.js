90f1490357ac9b30cd2b681192c9fa6a8cdafaa9
mhans003 <michaeledwardhanson@gmail.com>
2021-03-13 03:10:32 -0800

    Added extra information for insertion sort, including steps output and complexity/stability information.

--- js/sorts/Insertion.js
+++ js/sorts/Insertion.js
@@ -1,11 +1,13 @@
 function insertionSort(arr) {
     //Loop through each item in the array, starting at second.
     for(let currentIndex = 1; currentIndex < arr.length; currentIndex++) {
-        //Save the current value
+        //Save the current value (the one being compared to the items in the left sorted portion)
         let currentVal = arr[currentIndex];
 
-        //Loop through each item in the sorted portion, as long as the current value in the current sorted index is greater than the current value.
-        for(var sortedIndex = currentIndex - 1; sortedIndex >= 0 && arr[sortedIndex] > currentVal; sortedIndex--) {
+        //Loop through each item in the sorted portion(beginning) starting at the end of it, as long as the current value in the current sorted index is greater than the current value.
+        //Here, sortedIndex must be declared if using let, otherwise var must be used (we need to access after for loop)
+        let sortedIndex;
+        for(sortedIndex = currentIndex - 1; sortedIndex >= 0 && arr[sortedIndex] > currentVal; sortedIndex--) {
             document.getElementById("steps").innerHTML += `<li>shifting ${arr[sortedIndex]} into spot where ${arr[sortedIndex + 1]} is</li>`;
             //Push the item being looked at from sorted index into the next spot over.
             arr[sortedIndex + 1] = arr[sortedIndex];
